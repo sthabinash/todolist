e4dc93e69dea61494be5c6cf44343e463ccdd232
unknown <abinash@dt52.yomaridomain.com>
2013-07-24 16:35:36 +0545

    Added the add item editlist

--- gen/com/example/todolist/R.java
+++ gen/com/example/todolist/R.java
@@ -33,12 +33,14 @@ public final class R {
         public static final int Button03=0x7f090013;
         public static final int Button04=0x7f09000c;
         public static final int TextView01=0x7f090005;
-        public static final int action_settings=0x7f090019;
-        public static final int addlist=0x7f090018;
-        public static final int btn_add=0x7f090017;
+        public static final int action_settings=0x7f09001b;
+        public static final int addlist=0x7f09001a;
+        public static final int btn_add=0x7f090019;
         public static final int button1=0x7f090007;
         public static final int button3=0x7f090009;
-        public static final int months_list=0x7f090015;
+        public static final int button_add=0x7f090016;
+        public static final int editTodo=0x7f090015;
+        public static final int months_list=0x7f090017;
         public static final int root=0x7f09000d;
         public static final int signin_password=0x7f090006;
         public static final int signin_signin=0x7f090008;
@@ -48,7 +50,7 @@ public final class R {
         public static final int textView2=0x7f090001;
         public static final int textView3=0x7f090002;
         public static final int textView4=0x7f090003;
-        public static final int todo=0x7f090016;
+        public static final int todo=0x7f090018;
         public static final int up_password=0x7f09000f;
         public static final int up_repassword=0x7f090010;
         public static final int up_signin=0x7f090014;
--- src/com/example/todolist/Todoadpt.java
+++ src/com/example/todolist/Todoadpt.java
@@ -7,8 +7,10 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,43 +19,81 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Todoadpt extends Activity{
 	
 	  private ListView monthsListView;
-	  
+	  EditText to_add_item;
 	   private ArrayAdapter arrayAdapter;
-	  
+	   ArrayList <String>foundlist = new ArrayList<String>();
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Resources resources = this.getResources();
         setContentView(R.layout.todo_adpt);
-       
-        
+        add_todo_item();
+        to_add_item = (EditText)this.findViewById(R.id.editTodo);
        // String[] monthsArray = { "JAN", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY","AUG", "SEPT", "OCT", "NOV", "DEC" };
- 	
-        ArrayList <String>foundlist = new ArrayList<String>();
+        	Button add_btn = (Button)this.findViewById(R.id.button_add);
+			
+        	final EditText edittext = (EditText) findViewById(R.id.editTodo);
+		add_btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				//User us = new User("admin","admin");
+				String add_item = to_add_item.getText().toString();
+							
+				ApplicationSession.getUser().addtodolist(add_item); // add todo item to arraylist
+				to_add_item.setText("");
+				add_todo_item();  //call the adapter
+			}
+		});
+		
+		
+		edittext.setOnKeyListener(new OnKeyListener() {
+		    public boolean onKey(View v, int keyCode, KeyEvent event) {
+		        // If the event is a key-down event on the "enter" button
+		        if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+		            (keyCode == KeyEvent.KEYCODE_ENTER)) {
+		          // Perform action on key press
+		        	String add_item = to_add_item.getText().toString();
+					
+					ApplicationSession.getUser().addtodolist(add_item); // add todo item to arraylist
+					to_add_item.setText("");
+					add_todo_item();
+		          return true;
+		        }
+		        return false;
+		    }
+		});
+       
     
-    	foundlist = ApplicationSession.getUser().gettodolist();
-    //	String arr[]=foundlist.toArray(new String[foundlist.size()]);
     	
-    	
-        // Initialize the UI components
-        monthsListView = (ListView) findViewById(R.id.months_list);
-        // For this moment, you have ListView where you can display a list.
-        // But how can we put this data set to the list?
-        // This is where you need an Adapter
- 
-        // context - The current context.
-        // resource - The resource ID for a layout file containing a layout 
-                // to use when instantiating views.
-        // From the third parameter, you plugged the data set to adapter
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,foundlist);
-        
- 
-        // By using setAdapter method, you plugged the ListView with adapter
-        monthsListView.setAdapter(arrayAdapter);
     }
+	
+	protected void add_todo_item(){
+		foundlist = ApplicationSession.getUser().gettodolist();
+	    //	String arr[]=foundlist.toArray(new String[foundlist.size()]);
+	    	
+	    	
+	        // Initialize the UI components
+	        monthsListView = (ListView) findViewById(R.id.months_list);
+	        // For this moment, you have ListView where you can display a list.
+	        // But how can we put this data set to the list?
+	        // This is where you need an Adapter
+	 
+	        // context - The current context.
+	        // resource - The resource ID for a layout file containing a layout 
+	                // to use when instantiating views.
+	        // From the third parameter, you plugged the data set to adapter
+	        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,foundlist);
+	        
+	 
+	        // By using setAdapter method, you plugged the ListView with adapter
+	        monthsListView.setAdapter(arrayAdapter);
+	}
 
 }
